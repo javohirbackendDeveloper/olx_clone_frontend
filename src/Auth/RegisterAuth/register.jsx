595240c3cdede9fc70916e0d4d5102ca2f818c9a
Farrux-webs <ravshanbekovf02@gmail.com>
2025-02-09 07:58:52 +0500

    register page designed

--- src/Auth/RegisterAuth/register.jsx
+++ src/Auth/RegisterAuth/register.jsx
@@ -6,34 +6,22 @@ const Index = () => {
   return (
     <>
       <div className="register-wrapper">
+        <h2 className="register-heading">Sign Up</h2>
         <form className="register">
-          <h2 className="register-heading">Sign Up</h2>
-          <label htmlFor="username">
+          <label htmlFor="username"  className="data-input-wrapper">
             username
-            <input
-              type="text"
-              name="username"
-              className="data-input"
-            />
+            <input type="text" name="username" className="data-input" />
           </label>
-          <label htmlFor="username">
+          <label htmlFor="username" className="data-input-wrapper">
             Email
-            <input
-              type="text"
-              name="email"
-              className="data-input"
-            />
+            <input type="text" name="email" className="data-input" />
           </label>
-          <label htmlFor="username">
+          <label htmlFor="username" className="data-input-wrapper">
             password
-            <input
-              type="text"
-              name="password"
-              className="data-input"
-            />
+            <input type="text" name="password" className="data-input" />
           </label>
           <button type="submit" className="submit-register">
-            Login
+            Sign up
           </button>
         </form>
       </div>
