6ddef51803b27d800fe89fe9f9194f80f37d4046
Farrux-webs <ravshanbekovf02@gmail.com>
2025-02-15 05:25:28 +0500

    tailwind css intergrated

--- src/Auth/LoginAuth/Login.jsx
+++ src/Auth/LoginAuth/Login.jsx
@@ -5,18 +5,44 @@ import "../../Styles/auth/login.css";
 const index = () => {
   return (
     <>
-      <div className="login-wrapper">
+      <div className="login-wrapper relative min-h-screen flex items-center flex-col justify-center bg-gradient-to-br from-indigo-500 to-pink-500 overflow-hidden">
         <h2 className="login-heading">Log in</h2>
-        <form action="" className="login-form">
-          <label htmlFor="username" className="data-input-wrapper">
-            Username
-            <input type="text" name="username" className="data-input" />
+        <div class="absolute w-72 h-72 bg-white opacity-10 rounded-full -top-10 -left-10"></div>
+        <div class="absolute w-96 h-96 bg-white opacity-10 rounded-full -bottom-20 -right-20"></div>
+        <form className="login-form relative bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+          <label htmlFor="Username" className="data-input-wrapper text-black">
+            <p className="text-black cursor-pointer text-sm"> Username</p>
+            <input
+              type="text"
+              name="Username"
+              className="data-input block w-full px-4 py-2 text-gray-700 
+           bg-white border border-gray-300 rounded-sm
+           focus:border-blue-500 focus:ring-blue-500 focus:outline-none focus:ring 
+           transition duration-300 ease-in-out
+           shadow-sm hover:shadow-md"
+            />
           </label>
-          <label htmlFor="username" className="data-input-wrapper">
-            Password
-            <input type="text" name="password" className="data-input" />
+          <label htmlFor="Password" className="data-input-wrapper  text-black">
+            <p className="text-black cursor-pointer text-sm"> Password</p>
+            <input
+              type="text"
+              name="Password"
+              className="data-input block w-full px-4 py-2 text-gray-700 
+           bg-white border border-gray-300 rounded-sm
+           focus:border-blue-500 focus:ring-blue-500 focus:outline-none focus:ring 
+           transition duration-300 ease-in-out
+           shadow-sm hover:shadow-md"
+            />
           </label>
-          <button type="submit" className="submit-login">
+          <button
+            type="submit"
+            className="submit-login  px-6 py-2 text-white font-semibold 
+           bg-blue-600 rounded-sm shadow-md 
+           hover:bg-blue-700 hover:shadow-lg 
+           focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 
+           transition duration-300 ease-in-out 
+           cursor-pointer"
+          >
             Log in
           </button>
         </form>
